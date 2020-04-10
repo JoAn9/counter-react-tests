901117c5088ddf9306e019c118b2ee3d6d3c5dba
JoAn <joannacalinska@gmail.com>
2020-04-10 23:10:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  state = {
+    counter: 0,
+    error: false,
+  };
+
+  increment = () => {
+    this.setState({ error: false });
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  decrement = () => {
+    if (this.state.counter > 0) {
+      this.setState({ counter: this.state.counter - 1 });
+    } else {
+      this.setState({ error: true });
+    }
+  };
+  render() {
+    const errorClass = this.state.error ? '' : 'hidden';
+
+    return (
+      <div className="App" data-test="component-app">
+        <h1 data-test="counter-display">Counter is {this.state.counter}</h1>
+        <p data-test="error-message" className={`error ${errorClass}`}>
+          Can't be lower then 0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <button data-test="increment-button" onClick={this.increment}>
+          Increment
+        </button>
+        <button data-test="decrement-button" onClick={this.decrement}>
+          Decrement
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,124 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-test="component-app"]');
+
+  expect(appComponent.length).toBe(1);
+  // console.log(wrapper.debug());
+  // expect(wrapper).toBeTruthy();
+  // expect(wrapper.find('h1')).toHaveLength(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = shallow(<App />);
+  const counter = wrapper.find('[data-test="counter-display"]');
+
+  expect(counter.length).toBe(1);
+});
+
+test('counter strats at 0', () => {
+  const wrapper = shallow(<App />);
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
+});
+
+describe('Increment', () => {
+  test('renders increment button', () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find('[data-test="increment-button"]');
+    expect(button.length).toBe(1);
+  });
+
+  test('clicking button increments counter', () => {
+    const counter = 7;
+    const wrapper = shallow(<App />);
+    wrapper.setState({ counter });
+
+    const button = wrapper.find('[data-test="increment-button"]');
+    button.simulate('click');
+    wrapper.update();
+
+    const counterDisplay = wrapper.find('[data-test="counter-display"]');
+    expect(counterDisplay.text()).toContain(counter + 1);
+  });
+
+  test('clicking increment button clear error message', () => {
+    const wrapper = shallow(<App />);
+
+    const button = wrapper.find('[data-test="increment-button"]');
+    button.simulate('click');
+    wrapper.update();
+
+    const errorHasHiddenClass = wrapper
+      .find('[data-test="error-message"]')
+      .hasClass('hidden');
+    expect(errorHasHiddenClass).toBe(true);
+  });
+});
+
+describe('Decrement', () => {
+  test('renders debcrement button', () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find('[data-test="decrement-button"]');
+    expect(button.length).toBe(1);
+  });
+
+  test('clicking button decrements counter', () => {
+    const counter = 20;
+    const wrapper = shallow(<App />);
+    wrapper.setState({ counter });
+
+    const button = wrapper.find('[data-test="decrement-button"]');
+    button.simulate('click');
+    wrapper.update();
+
+    const counterDisplay = wrapper.find('[data-test="counter-display"]');
+    expect(counterDisplay.text()).toContain(counter - 1);
+  });
+
+  test("error msg doesn't show when no need", () => {
+    // const wrapper = setup();
+    // const errorMsg = findByTestAttr(wrapper, 'error-message');
+    const wrapper = shallow(<App />);
+    const errorHasHiddenClass = wrapper
+      .find('[data-test="error-message"]')
+      .hasClass('hidden');
+    expect(errorHasHiddenClass).toBe(true);
+  });
+});
+
+describe('Counter is 0', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+
+    const button = wrapper.find('[data-test="decrement-button"]');
+    button.simulate('click');
+    wrapper.update();
+  });
+
+  test('error shows', () => {
+    const errorHasHiddenClass = wrapper
+      .find('[data-test="error-message"]')
+      .hasClass('hidden');
+    expect(errorHasHiddenClass).toBe(false);
+  });
+
+  test('counter still displays 0', () => {
+    const counterDisplay = wrapper.find('[data-test="counter-display"]');
+    expect(counterDisplay.text()).toContain(0);
+  });
+
+  test('clicking button decrements counter but not lower then 0', () => {
+    const counterDisplay = wrapper.find('[data-test="counter-display"]');
+
+    expect(counterDisplay.text()).toContain(0);
+  });
 });
